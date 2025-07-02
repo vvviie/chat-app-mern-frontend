943be6c62c03df0ccb40d00b5b5c9bae1daa13c0
vvviie <jerickogomez22@gmail.com>
2025-07-02 15:35:01 +0800

    auth routes

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuthStore } from '../store/useAuthStore';
+import { MessageSquare } from 'lucide-react';
 
 const SignUpPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  })
+
+  const { signup, isSigningUp } = useAuthStore();
+
+  const validateForm = () => {
+
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>SignUpPage</div>
+    <div className="min-h-screen grid lg:grid-cols-2">
+      <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+        <div className="w-full max-w-md space-y-8">
+          {/*LOGO*/}
+          <div className="text-center mb-8">
+            <div className="flex flex-col items-center gap-2 group">
+              <div
+              className="size-12 rounded-xl bg-primary/10 flex items-center justify-center
+              group-hover:bg-primary/20 transition-colors"
+              >
+              <MessageSquare className="size-6 text-primary" />
+              </div>
+              <h1 className="text-2xl font-bold mt-2">Create Account</h1>
+              <p className="text-base-content/60">Get started with your free account</p>
+              </div>
+            </div>
+        </div>
+        //STOPPED AT 1:55:53
+        <form onSubmit={}></form>
+      </div>
+    </div>
   )
 }
 
--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -20,5 +20,9 @@ export const useAuthStore = create((set) => ({
     } finally {
       set({isCheckingAuth: false})
     }
+  },
+
+  signup: async (data) => {
+
   }
 }));
