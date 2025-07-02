83a247f8ccbadf4f322ff5154dfd724de061b49a
vvviie <jerickogomez22@gmail.com>
2025-07-02 11:48:57 +0800

    routing finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore'
 
 const Navbar = () => {
+
+    const { authUser } = useAuthStore()
+
   return (
     <div>Navbar</div>
   )
